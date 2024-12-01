5e7177b050ad44e8501391114163e5050d08266b
mohamed <mohamed.m.m.hafez@gmail.com>
2024-12-01 10:05:03 +0100

    add ActiveSupport::DescendantsTracker::VERSION

--- activesupport/lib/active_support/descendants_tracker.rb
+++ activesupport/lib/active_support/descendants_tracker.rb
@@ -7,6 +7,8 @@ module ActiveSupport
   # This module provides an internal implementation to track descendants
   # which is faster than iterating through ObjectSpace.
   module DescendantsTracker
+    VERSION = "simplified"
+
     @clear_disabled = false
 
     class << self
